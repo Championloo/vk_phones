6db862dfc8d4cb94bcb8c15d503f447037e55fc3
Championloo <championloo47@gmail.com>
2019-10-01 14:02:54 +0500

    Update vksrc.py

--- vksrc.py
+++ vksrc.py
@@ -18,7 +18,7 @@ except:
 		f.write(token)
 		print('Токен записан')
 print('Чтобы изменить токен, откройте файл token с помощью блокнота и перепишите значение или удалите файл\n')
-groupid = input('Введите ID или короткое имя (из ссылки) группы/паблика: ')
+groupid = input('Введите ID или короткое имя (из ссылки) группы/паблика: ').replace('https://vk.com/','')
 session = vk.Session(access_token=token)
 vk_api = vk.API(session)
 if groupid.isdigit()==False:
@@ -27,7 +27,7 @@ con = sqlite3.connect("{}.db".format(groupid))
 cursor = con.cursor()
 
 with con:
-	cursor.execute("""CREATE TABLE if not exists vk (id int, name text, last text, phone text)""")
+	cursor.execute("""CREATE TABLE if not exists vk (id int, name text, last text, phone text, city text)""")
 
 first = vk_api.groups.getMembers(group_id=groupid, v=5.92) 
 data = first["items"]  
@@ -36,7 +36,7 @@ for i in range(1, count+1):
 	data = data + vk_api.groups.getMembers(group_id=groupid, v=5.92, offset=i*1000)["items"]
 with con:
 	for d in data:
-		cursor.execute("INSERT INTO vk VALUES (?,?,?,?)",(str(d),'','',''))
+		cursor.execute("INSERT INTO vk VALUES (?,?,?,?,?)",(str(d),'','','',''))
 	con.commit()
 
 with con:
@@ -44,8 +44,9 @@ with con:
 	data = cursor.fetchall()
 	for i in tqdm(range(len(data)), smoothing=0, miniters=None):
 		try:
-			user = vk_api.users.get(user_ids=str(data[i][0]), v=5.92, fields='contacts', lang='ru')[0]
+			user = vk_api.users.get(user_ids=str(data[i][0]), v=5.92, fields='contacts, city', lang='ru')[0]
 			phone = ''
+			city = ''
 			if 'is_closed' in user and user['is_closed']==False:
 				if 'home_phone' in user and len(user['home_phone'])>8:
 					hphone = user['home_phone']
@@ -69,13 +70,16 @@ with con:
 							truephone += d
 					if len(truephone)>9:
 						phone += truephone+'; '
-			cursor.execute("UPDATE vk SET name=?, last=?, phone=? where id=?",(user['first_name'], user['last_name'], phone, str(data[i][0])))
+				if 'city' in user:
+					city = user['city']['title']
+			cursor.execute("UPDATE vk SET name=?, last=?, phone=?, city=? where id=?",(user['first_name'], user['last_name'], phone, city, str(data[i][0])))
 			con.commit()
 			time.sleep(0.001)
 		except:
 			time.sleep(15)
-			user = vk_api.users.get(user_ids=str(data[i][0]), v=5.92, fields='contacts', lang='ru')[0]
+			user = vk_api.users.get(user_ids=str(data[i][0]), v=5.92, fields='contacts, city', lang='ru')[0]
 			phone = ''
+			city = ''
 			if 'is_closed' in user and user['is_closed']==False:
 				if 'home_phone' in user and len(user['home_phone'])>8:
 					hphone = user['home_phone']
@@ -99,7 +103,9 @@ with con:
 							truephone += d
 					if len(truephone)>9:
 						phone += truephone+'; '
-			cursor.execute("UPDATE vk SET name=?, last=?, phone=? where id=?",(user['first_name'], user['last_name'], phone, str(data[i][0])))
+				if 'city' in user:
+					city = user['city']['title']
+			cursor.execute("UPDATE vk SET name=?, last=?, phone=?, city=? where id=?",(user['first_name'], user['last_name'], phone, city, str(data[i][0])))
 			con.commit()
 			time.sleep(0.001)
 
@@ -113,16 +119,20 @@ ws = wb.get_sheet('Лист1')
 ws.col(0).width = 5000
 ws.col(1).width = 10000
 ws.col(2).width = 7000
+ws.col(3).width = 7000
 ws.write(0, 0, 'Имя', style)
 ws.write(0, 1, 'Телефон', style)
-ws.write(0, 2, 'Ссылка на страницу', style)
+ws.write(0, 2, 'Ссылка на страницу (id)', style)
+ws.write(0, 3, 'Город', style)
 
 for d in range(1, len(data)):
-	if '' not in data[d]:
+	if ''!=data[d][3]:
+		print(data[d])
 		user = data[d]
 		ws.write(d, 0, user[1]+' '+user[2], style)
 		ws.write(d, 1, user[3], style)
 		ws.write(d, 2, 'https://vk.com/id'+str(user[0]), style)
+		ws.write(d, 3, user[4], style)
 wb.save('{}.xls'.format(groupid))
 con.close()
 os.remove("{}.db".format(groupid))
